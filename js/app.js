5b78fc3737a2525cfbab1a68cd65a9ee9e6194c8
Seitu <seitujoseph@gmail.com>
2019-09-02 15:08:44 -0400

    js for calculator operations functionality add

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
-function insert(num){
+var insert = (num) => {
   result.display.value += num;
+}
+var fraction = () => {
+  result.display.value += ".";
+}
+var remove = () => {
+  result.display.value = "";
+}
+//operators
+var sign = () => {
+  result.display.value *= -1;
+}
+var percent = () => {
+  result.display.value *= .01;
+}
+var divide = () => {
+  result.display.value += "/";
+}
+var multiply = () => {
+  result.display.value += "*";
+}
+var subtract = () => {
+  result.display.value += "-";
+}
+var add = () => {
+  result.display.value += "+";
+}
+//return
+var calculate = () => {
+  result.display.value = eval(result.display.value);
 }
